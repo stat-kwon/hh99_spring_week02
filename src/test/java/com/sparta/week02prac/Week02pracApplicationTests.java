5c0aaf29bdd6fbc5d4506a0276626b1c74fbc015
SEOLMIN KWON <stat_kwon@naver.com>
2021-06-27 19:18:35 +0900

    feat: spring CRUD기능 구현

---
+++ src/test/java/com/sparta/week02prac/Week02pracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.week02prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week02pracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
